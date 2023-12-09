ea415d957492c3a0f4944e54868153647ecfc253
misong <misong321@gmail.com>
2023-12-09 09:45:47 +0900

    feat: configure TerminusModule for health checks

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TerminusModule } from '@nestjs/terminus';
+import { HealthController } from './controllers/health.controller'; // Create this controller
+
+@Module({
+  imports: [TerminusModule],
+  controllers: [HealthController, AppController],
+  providers: [AppService],
+})
+export class AppModule {}
