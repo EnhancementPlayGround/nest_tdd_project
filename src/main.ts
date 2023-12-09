0398d7e1d65edb47c5fe1fc537ed288c8b1131af
misong <misong321@gmail.com>
2023-12-09 10:33:49 +0900

    chore: add start script and .env configuration

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,17 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  const phase = process.env.NODE_ENV || 'development';
+  const phase = process.env.NODE_ENV || 'dev';
   const envFileName = `.env.${phase}`;
+
   dotenv.config({ path: envFileName });
 
-  const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3001;
 
-  await app.listen(port);
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(port, () => {
+    console.log(`Server is running on port ${port} in ${phase} mode`);
+  });
 }
 bootstrap();
