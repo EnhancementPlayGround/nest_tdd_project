8530117ee6007859fc0b8218eea142b6bb3537b5
misong <misong321@gmail.com>
2023-12-24 22:54:14 +0900

    feat: Add CI/CD task to copy environment-specific configuration file

---
+++ src/services/healthcheck/health.controller.ts
@@ -0,0 +1,15 @@
+// health.controller.ts
+
+import { Controller, Get } from '@nestjs/common';
+import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
+
+@Controller('health')
+export class HealthController {
+  constructor(private readonly health: HealthCheckService) {}
+
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.health.check([]);
+  }
+}
