9f9c72dfa8a269de90b0fc7d5185317f557db24f
misong <misong321@gmail.com>
2024-01-01 22:47:41 +0900

    Refactor TypeORM Configuration for Environment-Based Loading
    - Extracted TypeORM configuration into a separate ormConfig.ts file.
    - Updated AppModule to load database configurations based on the NODE_ENV environment variable.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { TerminusModule } from '@nestjs/terminus';
 import { HealthController } from './services/healthcheck/health.controller';
 import { BalanceModule } from './services/balance/balance.module';
 import { ProductModule } from './services/product/product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ormconfig } from './config/ormConfig';
 
 @Module({
-  imports: [TerminusModule, BalanceModule, ProductModule],
+  imports: [
+    TerminusModule,
+    BalanceModule,
+    ProductModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ormconfig[process.env.NODE_ENV || '$default'], // 환경에 맞는 설정 사용
+    }),
+  ],
   controllers: [HealthController],
 })
 export class AppModule {}
